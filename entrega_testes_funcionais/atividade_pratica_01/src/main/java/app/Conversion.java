8e6ea805bee400aea21c5acebbdd6550f78d3b0d
Mayra <mayra.bernardesc@outlook.com>
2020-10-14 08:06:32 -0300

    Entrega 2 - Red State - TDD

---
+++ entrega_testes_funcionais/atividade_pratica_01/src/main/java/app/Conversion.java
@@ -0,0 +1,13 @@
+package app;
+
+import java.lang.*;
+import org.apache.commons.math3.util.FastMath;
+
+
+
+public class Conversion{
+    
+    public static Integer toMillimeters(Integer n) {
+		return 0;
+	}
+}
