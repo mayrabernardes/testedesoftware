20c6678a60e5991dd47048ef90fea2d0d74f2c7b
Mayra <mayra.bernardesc@outlook.com>
2020-10-14 08:50:20 -0300

    Entrega 2 - Blue State - TDD

--- entrega_testes_funcionais/atividade_pratica_01/src/main/java/app/Conversion.java
+++ entrega_testes_funcionais/atividade_pratica_01/src/main/java/app/Conversion.java
@@ -6,13 +6,15 @@ import org.apache.commons.math3.util.FastMath;
 public class Conversion{
     
     public static Integer toMillimeters(Integer n) {
-		if(0 < n && n < 999999){
+		if(n == 0){
+			return 0;
+		} else if(0 < n && n <= 999999){
 			int n1 = n.intValue();
 			int x = 1000;
 			int result = FastMath.multiplyExact(n1, x);//biblioteca de terceiros utilizada para a implementação
 			Integer result2 = new Integer(result);
 			return result2; //converte o numero para milimetros multiplicando por 1000
-		} else {
+		} else{
 			return null;
 		}
 	}
